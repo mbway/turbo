bc2ebddb7e0891491baa7410db9759c2e92f6021
mbway <matthewbroadway@msn.com>
2017-09-05 21:04:00 +0100

    moved things around

---
+++ optimisation/__init__.py
@@ -0,0 +1,8 @@
+
+from utils import *
+from core import Job, Sample, Evaluator, Optimiser
+from basic_optimisers import GridSearchOptimiser, RandomSearchOptimiser
+from bayesian_optimiser import BayesianOptimisationOptimiser
+import gui
+import net
+
